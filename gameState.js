24a3de17c7cff898b044fcf61eb28e66ed338870
Paul-CodeGH <paul.moldovan12345@yahoo.com>
2025-02-03 17:53:40 +0100

    Add files via upload

---
+++ gameState.js
@@ -0,0 +1,7 @@
+
+export const global = {
+    speedBoostApplied: false,
+    jumpBoostApplied: false,
+    lastTime: 0,
+};
+// Global object to store game state, such as speed boost and jump boost
